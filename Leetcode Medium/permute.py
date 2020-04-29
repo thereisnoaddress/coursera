4c2dbc19856b0d23e41149dea79c48275744ccbd
Chris <thereisnoaddress@gmail.com>
2020-04-29 13:56:49 -0400

    Completed leftrotation

---
+++ Leetcode Medium/permute.py
@@ -0,0 +1,20 @@
+"""
+Given a collection of distinct integers, return all possible permutations.
+
+Example:
+
+Input: [1,2,3]
+Output:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+"""
+
+def permute(nums: List[int]) -> List[List[int]]:
+    # consider every integer as a node in a tree
+    # do DFS
