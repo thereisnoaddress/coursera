940048557e2a12675bb5b9402b99e9d149e6d322
Chris <thereisnoaddress@gmail.com>
2020-05-04 15:18:55 -0400

    Finally solved permutation! Need to work on recursion-related questions

--- Leetcode Medium/permute.py
+++ Leetcode Medium/permute.py
@@ -15,6 +15,31 @@ Output:
 ]
 """
 
-def permute(nums: List[int]) -> List[List[int]]:
-    # consider every integer as a node in a tree
-    # do DFS
+def permute(nums):
+  # consider every integer as a node in a tree
+  # do DFS
+  # some sort of backtracking recursive algorithm
+  cur = []
+  rem = nums[:]
+  allPerm = []
+  permutation(cur, rem, allPerm)
+  return allPerm
+  
+
+def permutation(cur, remaining, allPerm): 
+  # cur = array of currently selected element
+  if len(remaining) == 0: 
+    allPerm.append(cur)
+  else: 
+    for item in remaining:   
+      curCopy = cur[:]
+      curCopy.append(item)
+      remCopy = remaining[:]
+      remCopy.remove(item)
+      permutation(curCopy, remCopy, allPerm)
+    
+
+
+if __name__ == "__main__":
+    allPermutations = permute([1,2,3,4])
+    print(allPermutations)
