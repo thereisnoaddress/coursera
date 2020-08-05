4ba447991c891fea18edc078ee0884336ecd29fb
Chris Zhu <chris@Chriss-MacBook-Pro.local>
2020-08-05 12:06:33 -0400

    Finished 1169

---
+++ Leetcode Medium/invalidTransactions.py
@@ -0,0 +1,66 @@
+"""
+1169
+https://leetcode.com/problems/invalid-transactions/
+
+A transaction is possibly invalid if:
+
+the amount exceeds $1000, or;
+if it occurs within (and including) 60 minutes of another transaction with the same name in a different city.
+Each transaction string transactions[i] consists of comma separated values representing the name, time (in minutes), amount, and city of the transaction.
+
+Given a list of transactions, return a list of transactions that are possibly invalid.  You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: transactions = ["alice,20,800,mtv","alice,50,100,beijing"]
+Output: ["alice,20,800,mtv","alice,50,100,beijing"]
+Explanation: The first transaction is invalid because the second transaction occurs within a difference of 60 minutes, have the same name and is in a different city. Similarly the second one is invalid too.
+Example 2:
+
+Input: transactions = ["alice,20,800,mtv","alice,50,1200,mtv"]
+Output: ["alice,50,1200,mtv"]
+Example 3:
+
+Input: transactions = ["alice,20,800,mtv","bob,50,1200,mtv"]
+Output: ["bob,50,1200,mtv"]
+ 
+
+Constraints:
+
+transactions.length <= 1000
+Each transactions[i] takes the form "{name},{time},{amount},{city}"
+Each {name} and {city} consist of lowercase English letters, and have lengths between 1 and 10.
+Each {time} consist of digits, and represent an integer between 0 and 1000.
+Each {amount} consist of digits, and represent an integer between 0 and 2000.
+"""
+
+class Solution:
+    def invalidTransactions(self, transactions):
+        
+        invalids = []
+        # name: [time, location]
+        prevTrans = {}
+        
+        for transaction in transactions: 
+            name, time, price, location = transaction.split(",")
+            
+            if int(price) > 1000: 
+                invalids.append(transaction)
+            
+            if name in prevTrans: 
+                for trans in prevTrans[name]: 
+                    prevName, prevTime, prevPrice, prevLoc = trans.split(",")
+                    if abs(int(prevTime) - int(time)) <= 60 and prevLoc != location: 
+                        invalids.append(trans)
+                        invalids.append(transaction)
+
+            if name in prevTrans: 
+                prevTrans[name].append(transaction)
+            else:
+                prevTrans[name] = [transaction]
+        
+        return set(invalids)
+            
+            
