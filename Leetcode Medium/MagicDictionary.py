531c54b8f7611ca078e6e7635b56426f89f89b6b
Chris Zhu <chris@Chriss-MacBook-Pro.local>
2020-08-22 15:28:34 -0400

    Another way of doing magicDictionary

--- Leetcode Medium/MagicDictionary.py
+++ Leetcode Medium/MagicDictionary.py
@@ -1,5 +1,6 @@
 """
-Leetcode 676 Magic Dictionary implemented in two ways
+Leetcode 676 Magic Dictionary implemented in two ways:
+First, implemented as a Trie
 """
 
 class TrieNode: 
@@ -28,7 +29,13 @@ class MagicDictionary:
         for word in dictionary: 
             self.addWord(word)
     
-
+    """
+    Recursive function
+        - if the word is empty, check if all chances have been used up and whether it's at the end
+        - check whether word initial matches with one of the keys; if so, continue to next char
+            - if word initial doesn't match up with one of the keys, use up a chance (assume that char is being substituted)
+            - continue searching but with 0 chances left
+    """
     def newSearch(self, chances, node, word): 
         if not word: 
             return chances == 0 and node.isEnd
@@ -53,3 +60,35 @@ class MagicDictionary:
 # obj = MagicDictionary()
 # obj.buildDict(dictionary)
 # param_2 = obj.search(searchWord)
+################################### Done using dictionary -- more intuitive
+
+class MagicDictionary2:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.lengthDict = {}
+        # key: length -> value: String[]
+
+    def buildDict(self, dictionary) -> None:
+        # put words with the same length together in a dictionary
+        for word in dictionary: 
+            length = len(word)
+            if length not in self.lengthDict: 
+                self.lengthDict[length] = [word]
+            else: 
+                self.lengthDict[length].append(word)
+
+    def search(self, searchWord: str) -> bool:
+        if len(searchWord) not in self.lengthDict: return False
+        words = self.lengthDict[len(searchWord)]
+        
+        for word in words: 
+            diff = 0
+            for i in range(len(word)): 
+                if word[i] != searchWord[i]:
+                    diff += 1
+            if diff == 1: 
+                return True
+        return False
