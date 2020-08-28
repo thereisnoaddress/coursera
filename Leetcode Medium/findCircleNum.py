725b24cc5e18c9dcdc4c9c4fbd0228f1709b0efb
Chris Zhu <chris@Chriss-MacBook-Pro.local>
2020-08-28 14:31:21 -0400

    Find circle num

---
+++ Leetcode Medium/findCircleNum.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findCircleNum(self, M: List[List[int]]) -> int:
+        
+       
+        
+        def find(x): 
+            if circles[x] == x: 
+                return circles[x]
+            return find(circles[x])
+        
+        def union(a, b): 
+            leaderA = find(a)
+            leaderB = find(b)
+            
+            circles[leaderB] = leaderA
+        
+        circles = list(range(len(M)))
+        for i in range(len(M)): 
+            for j in range(i): 
+                if M[i][j]: 
+                    union(i, j)
+
+        return len(set(find(i) for i in range(len(M))))
+        
+        
