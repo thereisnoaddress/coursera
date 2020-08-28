c8c20aba0fb780d230c362fc570506c139b60a56
Chris Zhu <chris@Chriss-MacBook-Pro.local>
2020-08-28 16:51:45 -0400

    Find redundant connection

---
+++ Leetcode Medium/findRedundantConnection.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        # union find solution 
+        collection = [0] * len(edges)
+        
+        def find(edge): 
+            if collection[edge] == 0: 
+                return edge
+            return find(collection[edge])
+                
+        def union(e1, e2): 
+            e1Root = find(e1)
+            e2Root = find(e2)
+            if e1Root == e2Root: 
+                return False
+            collection[e1Root] = e2Root
+            return True
+        
+        for e1, e2 in edges: 
+            if not union(e1 - 1, e2 - 1): 
+                return [e1, e2]
